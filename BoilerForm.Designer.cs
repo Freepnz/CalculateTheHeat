bfa97e59106b84f62f0018ac90d4e9069e177d18
Дмитрий Дмитриевич Столяров <62957218+Freepnz@users.noreply.github.com>
2020-11-04 10:47:34 +0300

    Сделал BoilerForm. Закончил дизайн расчёта котла по площади дома.

--- BoilerForm.Designer.cs
+++ BoilerForm.Designer.cs
@@ -31,16 +31,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BoilerForm));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.labelCaclulateAreaResult = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelPoverReserve = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelAreaHouse = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.labelAreaHouse = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.labelPoverReserve = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.labelCaclulateAreaResult = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -76,6 +76,86 @@
             this.tabPage1.Text = "Расчёт по площади";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // labelCaclulateAreaResult
+            // 
+            this.labelCaclulateAreaResult.AutoSize = true;
+            this.labelCaclulateAreaResult.Location = new System.Drawing.Point(99, 330);
+            this.labelCaclulateAreaResult.Name = "labelCaclulateAreaResult";
+            this.labelCaclulateAreaResult.Size = new System.Drawing.Size(37, 13);
+            this.labelCaclulateAreaResult.TabIndex = 8;
+            this.labelCaclulateAreaResult.Text = "Result";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(96, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(577, 32);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Если ваш случай отличается от среднестатистичекого дома, \r\nстоит рассчитать мощно" +
+    "сть котла более точным методом с учетом всех особенностей.";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10",
+            "15",
+            "20",
+            "25"});
+            this.comboBox1.Location = new System.Drawing.Point(222, 198);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // labelPoverReserve
+            // 
+            this.labelPoverReserve.AutoSize = true;
+            this.labelPoverReserve.Location = new System.Drawing.Point(96, 201);
+            this.labelPoverReserve.Name = "labelPoverReserve";
+            this.labelPoverReserve.Size = new System.Drawing.Size(107, 13);
+            this.labelPoverReserve.TabIndex = 5;
+            this.labelPoverReserve.Text = "Запас мощности, %";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // labelAreaHouse
+            // 
+            this.labelAreaHouse.AutoSize = true;
+            this.labelAreaHouse.Location = new System.Drawing.Point(96, 162);
+            this.labelAreaHouse.Name = "labelAreaHouse";
+            this.labelAreaHouse.Size = new System.Drawing.Size(103, 13);
+            this.labelAreaHouse.TabIndex = 3;
+            this.labelAreaHouse.Text = "Площадь дома, м2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(86, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(93, 84);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(195, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(519, 64);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Наиболее простой и распространенный способ. \r\nИсходя из практики, для среднестати" +
+    "стического частного дома в \r\nклиматической зоне Подмосковья, с кладкой в 2 кирпи" +
+    "ча и высотой \r\nпотолков 2,7 м.\r\n";
+            // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
@@ -96,93 +176,13 @@
             this.tabPage3.Text = "Расчёт с учётом все особенностей";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(195, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(519, 64);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Наиболее простой и распространенный способ. \r\nИсходя из практики, для среднестати" +
-    "стического частного дома в \r\nклиматической зоне Подмосковья, с кладкой в 2 кирпи" +
-    "ча и высотой \r\nпотолков 2,7 м.\r\n";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(86, 21);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(93, 84);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // labelAreaHouse
-            // 
-            this.labelAreaHouse.AutoSize = true;
-            this.labelAreaHouse.Location = new System.Drawing.Point(96, 162);
-            this.labelAreaHouse.Name = "labelAreaHouse";
-            this.labelAreaHouse.Size = new System.Drawing.Size(103, 13);
-            this.labelAreaHouse.TabIndex = 3;
-            this.labelAreaHouse.Text = "Площадь дома, м2";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(222, 159);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 4;
-            // 
-            // labelPoverReserve
-            // 
-            this.labelPoverReserve.AutoSize = true;
-            this.labelPoverReserve.Location = new System.Drawing.Point(96, 201);
-            this.labelPoverReserve.Name = "labelPoverReserve";
-            this.labelPoverReserve.Size = new System.Drawing.Size(107, 13);
-            this.labelPoverReserve.TabIndex = 5;
-            this.labelPoverReserve.Text = "Запас мощности, %";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "10",
-            "15",
-            "20",
-            "25"});
-            this.comboBox1.Location = new System.Drawing.Point(222, 198);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(100, 21);
-            this.comboBox1.TabIndex = 6;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(96, 273);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(577, 32);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Если ваш случай отличается от среднестатистичекого дома, \r\nстоит рассчитать мощно" +
-    "сть котла более точным методом с учетом всех особенностей.";
-            // 
-            // labelCaclulateAreaResult
-            // 
-            this.labelCaclulateAreaResult.AutoSize = true;
-            this.labelCaclulateAreaResult.Location = new System.Drawing.Point(99, 330);
-            this.labelCaclulateAreaResult.Name = "labelCaclulateAreaResult";
-            this.labelCaclulateAreaResult.Size = new System.Drawing.Size(35, 13);
-            this.labelCaclulateAreaResult.TabIndex = 8;
-            this.labelCaclulateAreaResult.Text = "label3";
-            // 
-            // CalculateBoiler
+            // BoilerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tabControl1);
-            this.Name = "CalculateBoiler";
+            this.Name = "BoilerForm";
             this.Text = "Расчёт бойлера";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
